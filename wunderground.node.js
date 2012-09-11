5cccae0ef5c3c445fc90ef28b66e4852fd06019e
Christoffer Torris Olsen <chris@ctolsen.no>
2012-09-11 05:13:11 +0200

    Adding node.js script for weather update

--- wunderground.node.js
+++ wunderground.node.js
@@ -70,9 +70,18 @@ stations.forEach( function(station) {
         res.on('data', function(chunk) {
           var objname = "planner" + date.toString();
           //console.log(objname);
-          doc = cons(objname, JSON.parse(chunk));
-          db.raw_weathers.update({"station":station.code},{$set:doc}, function(err) {});
-          console.log("Saved " + station.code)
+          try {
+            doc = cons(objname, JSON.parse(JSON.minify(chunk)));
+          } catch(err) {
+            console.log("Error in " + station.code + ", " + err)
+          }
+          try {
+          db.raw_weathers.update({"station":station.code},{$set:doc}, function(err) {
+            
+          });
+        } catch (err) {
+          console.log("Error in " + station.code + " " + objname + ", " + err)
+        }
           //console.log("Got it.");
         }).on('error', function(e) {
           console.log("Got error: " + e.message);
@@ -85,3 +94,60 @@ stations.forEach( function(station) {
 
 };
 setTimeout(function () { setup() }, 5000);
+
+(function(global){
+	if (typeof global.JSON == "undefined" || !global.JSON) {
+		global.JSON = {};
+	}
+
+	global.JSON.minify = function(json) {
+
+		var tokenizer = /"|(\/\*)|(\*\/)|(\/\/)|\n|\r/g,
+			in_string = false,
+			in_multiline_comment = false,
+			in_singleline_comment = false,
+			tmp, tmp2, new_str = [], ns = 0, from = 0, lc, rc
+		;
+
+		tokenizer.lastIndex = 0;
+
+		while (tmp = tokenizer.exec(json)) {
+			lc = RegExp.leftContext;
+			rc = RegExp.rightContext;
+			if (!in_multiline_comment && !in_singleline_comment) {
+				tmp2 = lc.substring(from);
+				if (!in_string) {
+					tmp2 = tmp2.replace(/(\n|\r|\s)*/g,"");
+				}
+				new_str[ns++] = tmp2;
+			}
+			from = tokenizer.lastIndex;
+
+			if (tmp[0] == "\"" && !in_multiline_comment && !in_singleline_comment) {
+				tmp2 = lc.match(/(\\)*$/);
+				if (!in_string || !tmp2 || (tmp2[0].length % 2) == 0) {	// start of string with ", or unescaped " character found to end string
+					in_string = !in_string;
+				}
+				from--; // include " character in next catch
+				rc = json.substring(from);
+			}
+			else if (tmp[0] == "/*" && !in_string && !in_multiline_comment && !in_singleline_comment) {
+				in_multiline_comment = true;
+			}
+			else if (tmp[0] == "*/" && !in_string && in_multiline_comment && !in_singleline_comment) {
+				in_multiline_comment = false;
+			}
+			else if (tmp[0] == "//" && !in_string && !in_multiline_comment && !in_singleline_comment) {
+				in_singleline_comment = true;
+			}
+			else if ((tmp[0] == "\n" || tmp[0] == "\r") && !in_string && !in_multiline_comment && in_singleline_comment) {
+				in_singleline_comment = false;
+			}
+			else if (!in_multiline_comment && !in_singleline_comment && !(/\n|\r|\s/.test(tmp[0]))) {
+				new_str[ns++] = tmp[0];
+			}
+		}
+		new_str[ns++] = rc;
+		return new_str.join("");
+	};
+})(this);
